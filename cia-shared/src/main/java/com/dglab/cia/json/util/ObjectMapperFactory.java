ddb6601f2196e4d71a11d419ec6be6496d65848e
doctorgester <doctorgester@gmail.com>
2016-09-27 18:06:29 +0300

    Do not fail on missing creator properties

--- cia-shared/src/main/java/com/dglab/cia/json/util/ObjectMapperFactory.java
+++ cia-shared/src/main/java/com/dglab/cia/json/util/ObjectMapperFactory.java
@@ -17,7 +17,7 @@ public class ObjectMapperFactory {
     public static ObjectMapper createObjectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
-        mapper.configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, true);
+        mapper.configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, false);
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
         mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
