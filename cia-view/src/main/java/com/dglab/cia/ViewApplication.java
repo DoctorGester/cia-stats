7cf41a611b97e10823cf60a4c1de604e305ee0cf
doctorgester <doctorgester@gmail.com>
2016-10-25 18:31:02 +0400

    Fixed password

--- cia-view/src/main/java/com/dglab/cia/ViewApplication.java
+++ cia-view/src/main/java/com/dglab/cia/ViewApplication.java
@@ -57,11 +57,7 @@ public class ViewApplication {
 		port(80);
 		threadPool(4);
 
-        try {
-            secure("keystore.jks", FileUtils.readFileToString(new File("private.key")), null, null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        secure("keystore.jks", "", null, null);
 
         mapGet("/ranks/top/:mode", "ranks/top/byMode", new TypeReference<List<RankedPlayer>>(){});
         mapGet("/", "home", new TypeReference<AllStats>(){});
